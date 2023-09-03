ed5b70e2ddb529df893f15fbcc676b100b4cde35
ahmad <daloona26@gmail.com>
2023-09-03 17:02:12 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const durationInput = document.querySelector("#duration");
+const startBtn = document.getElementById("start");
+const pauseBtn = document.getElementById("pause");
+const circle = document.querySelector("circle");
+const perimeter = circle.getAttribute("r") * 2 * Math.PI;
+circle.setAttribute("stroke-dasharray", perimeter);
+
+let duration;
+const timer = new Timer(durationInput, startBtn, pauseBtn, {
+  onStart(totalDuration) {
+    duration = totalDuration;
+  },
+  onTick(timeRemaining) {
+    circle.setAttribute(
+      "stroke-dashoffset",
+      (perimeter * timeRemaining) / duration - perimeter // perimeter * timeRemaining / totalDuration - perimeter
+    );
+  },
+  onComplete() {
+    console.log("Timer is completed");
+  },
+});
---
+++ timer.js
@@ -0,0 +1,53 @@
+class Timer {
+  constructor(durationInput, startBtn, pauseBtn, callbacks) {
+    this.durationInput = durationInput;
+    this.startBtn = startBtn;
+    this.pauseBtn = pauseBtn;
+
+    if (callbacks) {
+      this.onStart = callbacks.onStart;
+      this.onTick = callbacks.onTick;
+      this.onComplete = callbacks.onComplete;
+    }
+
+    this.timeLeft = 30;
+
+    this.startBtn.addEventListener("click", this.start);
+    this.pauseBtn.addEventListener("click", this.pause);
+  }
+
+  start = () => {
+    if (this.onStart) {
+      this.onStart(this.timeRemaining);
+    }
+    this.interval = setInterval(this.tick, 20);
+  };
+
+  pause = () => {
+    clearInterval(this.interval);
+  };
+
+  onDurationChange() {}
+
+  tick = () => {
+    if (this.timeRemaining <= 0) {
+      this.pause();
+      if (this.onComplete) {
+        this.onComplete();
+      }
+    } else {
+      this.timeRemaining -= 0.02;
+      if (this.onTick) {
+        this.onTick(this.timeRemaining);
+      }
+    }
+  };
+
+  get timeRemaining() {
+    return parseFloat(this.durationInput.value);
+  }
+
+  set timeRemaining(time) {
+    this.durationInput.value = time.toFixed(2);
+  }
+}
